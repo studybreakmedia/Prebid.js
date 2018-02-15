46f6569699f0505365dcc78efe7db462ae1dd806
Mike Miller <mmiller@chegg.com>
2018-02-15 16:48:31 -0500

    Use correct variable name

--- modules/openxBidAdapter.js
+++ modules/openxBidAdapter.js
@@ -109,7 +109,7 @@ export const spec = {
           dealId: null,
           currency: 'USD',
           netRevenue: true,
-          mediaType: _mediaTypes.BANNER,
+          mediaType: BANNER,
           ttl: 60000,
           ad: null
         })
